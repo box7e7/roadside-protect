9e5d25f3a99deea3652f5950f6b39574d0dca0be
Mehdi <parislight#live.com>
2024-01-10 22:18:54 -0600

    first commit

--- components/Map.js
+++ components/Map.js
@@ -2,7 +2,7 @@ import React,{useContext} from "react";
 import { useRouter } from 'next/router'   
 import Context from "../components/ContextFile";
 import GoogleMapReact from 'google-map-react';
-
+    
 
 
 
